c0d01b2f37556fa13031b31600e5ef205c99fec5
fadilhakim <fadil.nylon@gmail.com>
2018-10-29 22:58:14 +0800

    contact form

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const http = require('http');
 const routes = require('./routes/routes.js')
 const app = express()
+const nodemailer = require('nodemailer');
 
 const helmet = require('helmet');
 const compression = require('compression');
@@ -11,6 +12,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const i18n=require("i18n-express");
 
+const flash = require('express-flash-messages')
+
+app.use(flash())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(i18n({
@@ -29,8 +33,36 @@ app.use(helmet(), compression(), cookieParser());
 
 
 app.get('/maintenance', (req, res) => res.render('maintenance'))
+app.get('/test', (req, res) => res.render('contact-success'))
 app.get('/', (req, res) => res.render('home'))
 app.get('/why-us', (req, res) => res.render('why-us'))
+
+app.post('/contact', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: process.env.GMAIL_USER,
+    subject: 'New message from potential client',
+    html: `<strong>Client Name :</strong> ${req.body.name} </br> <strong>Client Email :</strong> ${req.body.email} </br> <strong>Service Needs :</strong> ${req.body.category} </br> <strong>Time to Meetup :</strong> ${req.body.date} </br> <strong>The Detail :</strong> ${req.body.detail}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-success')
+    }
+    else {
+      res.render('contact-success')
+    }
+  });
+});
 
 routes(app);
 
